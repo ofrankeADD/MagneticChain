b4b960d247ec87a6e4942c160b11c9a1ed5ab0e3
ofrankeADD <49908052+ofrankeADD@users.noreply.github.com>
2021-11-22 18:02:27 +0100

    comments in main plus author date...

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+/*
+author: Oliver Franke
+email: ofranke@posteo.de
+github: ofrankeADD
+latest update: 22.11.2021
+*/
+
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
@@ -9,6 +16,26 @@ using namespace std;
 #include "constants.h"
 #include "dataFileHandling.hpp"
 #include "metropolis.hpp"
+
+/*
+The initial conditions are build as follows:
+- create m cubes aligned along the y-axis in the xy-plane
+- each cube consists of n^3 equidistantly placed dipoles
+- each dipole has an magnetic momentum vector initially pointing in the y-direction
+- the total magnetic chain consists of m x n^3 "elemental" dipoles
+
+The Metropolis Monte Carlo Algorithm is executed as follows:
+- calculate the total energy for all individual dipole-dipole interactions of all cubes in the chain
+- for each Monte Carlo step, either rotate or translate a randomly selected cube
+- check that the updated cube position is not colliding with any other cubic volume
+- calculate the updated total energy budget
+- if the new chain configuration is more energetically favourable than the old one, keep it
+- otherwise within a certain probability, discard the new chain configuration
+
+The data output is written at every stepping time as a snapshot containing the position vectors and the energy value.
+- the Python script is used in order to visualize the movement of the cubic chain, either for one snapshot or for all in form of a movie
+- the input parameters for the Python script have to be: n, 'all', m, stepsize, total_runs
+*/
 
 int main()
 {
--- src/visualize_dipVecsCoords.py
+++ src/visualize_dipVecsCoords.py
@@ -24,92 +24,101 @@ def set_axes_equal(ax, zoom=0.5):
     ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
     ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
 
+    return x_middle, y_middle, z_middle, plot_radius
+
 def get_cmap(n, name='hsv'):
     # Returns a function that maps each index in n to a distinct color
     return plt.cm.get_cmap(name, n)
 
 def make_movie_all(numElem, chainSize, steps, runs):
-	filename_movie = "../res/dipVecsCoords_all_c"+str(chainSize)+"_e"+str(numElem)+"_r"+str(runs)+"_s"+str(steps)
-	filename = "../res/snaps/frame_"+"_c"+str(chainSize)+"_e"+str(numElem)+"_r"+str(runs)+"_s"+str(steps)
+    zoom = 0.3
+    x_middle = y_middle = z_middle = plot_radius = 0.0
+    filename_movie = "../res/dipVecsCoords_all_c"+str(chainSize)+"_e"+str(numElem)+"_r"+str(runs)+"_s"+str(steps)
+    filename = "../res/snaps/frame_"+"_c"+str(chainSize)+"_e"+str(numElem)+"_r"+str(runs)+"_s"+str(steps)+"_z"+str(zoom)
 
-	#subprocess.call('rm ../res/snaps/*', shell=True)
+    #subprocess.call('rm ../res/snaps/*', shell=True)
 
-	step = 0
-	with open(filename_movie+".txt") as f:
-		while step < steps:
-			if step == 0:
-				line = f.readline()
-			if line[:4] == '#run':
-				step += 1
-				line = f.readline()
+    step = 0
+    with open(filename_movie+".txt") as f:
+        while step < steps:
+            if step == 0:
+                line = f.readline()
+            if line[:4] == '#run':
+                step += 1
+                line = f.readline()
 
-				coords = []
-				while line[:4] != '#run':
-					if line[0] not in ['#', ' ', '\n']:
-						line = line[:-1]
-						coords.append(list(map(float, line.split('\t'))))
-					line = f.readline()
-					if not line:
-						break
+                coords = []
+                while line[:4] != '#run':
+                    if line[0] not in ['#', ' ', '\n']:
+                        line = line[:-1]
+                        coords.append(list(map(float, line.split('\t'))))
+                    line = f.readline()
+                    if not line:
+                        break
 
-				coords = np.array(coords)
+                coords = np.array(coords)
 
-				fig = plt.figure()
-				ax = fig.add_subplot(111, projection='3d')
-				cmap = get_cmap(chainSize)
-				for c in range(chainSize):
-					if c == 0:
-						ax.scatter(coords[:numElem**3,1], coords[:numElem**3,2], coords[:numElem**3,3], color=cmap(c))
-					elif c == chainSize-1:
-						ax.scatter(coords[numElem**3*c:,1], coords[numElem**3*c:,2], coords[numElem**3*c:,3], color=cmap(c))
-					else:
-						ax.scatter(coords[numElem**3*c:numElem**3*(c+1),1], coords[numElem**3*c:numElem**3*(c+1),2], coords[numElem**3*c:numElem**3*(c+1),3], color=cmap(c))
-				ax.set_xlabel('x')
-				ax.set_ylabel('y')
-				ax.set_zlabel('z')
-				set_axes_equal(ax, zoom=0.1)
-				#plt.show()
+                fig = plt.figure()
+                ax = fig.add_subplot(111, projection='3d')
+                cmap = get_cmap(chainSize)
+                for c in range(chainSize):
+                    if c == 0:
+                        ax.scatter(coords[:numElem**3,1], coords[:numElem**3,2], coords[:numElem**3,3], color=cmap(c))
+                    elif c == chainSize-1:
+                        ax.scatter(coords[numElem**3*c:,1], coords[numElem**3*c:,2], coords[numElem**3*c:,3], color=cmap(c))
+                    else:
+                        ax.scatter(coords[numElem**3*c:numElem**3*(c+1),1], coords[numElem**3*c:numElem**3*(c+1),2], coords[numElem**3*c:numElem**3*(c+1),3], color=cmap(c))
+                ax.set_xlabel('x')
+                ax.set_ylabel('y')
+                ax.set_zlabel('z')
+                if step == 1:
+                    x_middle, y_middle, z_middle, plot_radius = set_axes_equal(ax, zoom=zoom)
+                else:
+                    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+                    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+                    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+                #plt.show()
 
-				fig.savefig(filename+"_zoom_%03d.png"%(step-1), dpi=300)
-				plt.close(fig)
+                fig.savefig(filename+"_%03d.png"%(step-1), dpi=300)
+                plt.close(fig)
 
-	subprocess.call("ffmpeg -y -framerate 10 -i "+filename+"_zoom_%03d.png -pix_fmt yuv420p -vf scale=1280:-2 "+filename_movie+"_zoom_movie.mp4", shell=True)
-	subprocess.call("ffmpeg -y -i "+filename_movie+"_zoom_movie.mp4 -vf 'fps=10,scale=340:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse' -loop 0 "+filename_movie+"_zoom_movie.gif", shell=True)
+    subprocess.call("ffmpeg -y -framerate 10 -i "+filename+"_%03d.png -pix_fmt yuv420p -vf scale=1280:-2 "+filename_movie+"_z"+str(zoom)+"_movie.mp4", shell=True)
+    subprocess.call("ffmpeg -y -i "+filename_movie+"_z"+str(zoom)+"_movie.mp4 -vf 'fps=10,scale=340:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse' -loop 0 "+filename_movie+"_z"+str(zoom)+"_movie.gif", shell=True)
 
 def main(numElem, name, chainSize, steps, runs):
-	if name == 'all':
-		make_movie_all(numElem, chainSize, steps, runs)
+    if name == 'all':
+        make_movie_all(numElem, chainSize, steps, runs)
 
-	else:
-		filename = "../res/dipVecsCoords_"+name+"_c"+str(chainSize)+"_e"+str(numElem)+"_r"+str(runs)+"_s"+str(steps)
-		coords = []
-		with open(filename+".txt") as f:
-			for line in f:
-				if line[0] not in ['#', ' ', '\n']:
-					line = line[:-1]
-					coords.append(list(map(float, line.split('\t'))))
+    else:
+        filename = "../res/dipVecsCoords_"+name+"_c"+str(chainSize)+"_e"+str(numElem)+"_r"+str(runs)+"_s"+str(steps)
+        coords = []
+        with open(filename+".txt") as f:
+            for line in f:
+                if line[0] not in ['#', ' ', '\n']:
+                    line = line[:-1]
+                    coords.append(list(map(float, line.split('\t'))))
 
-		coords = np.array(coords)
+        coords = np.array(coords)
 
-		fig = plt.figure()
-		ax = fig.add_subplot(111, projection='3d')
-		cmap = get_cmap(chainSize)
-		for c in range(chainSize):
-			if c == 0:
-				ax.scatter(coords[:numElem**3,0], coords[:numElem**3,1], coords[:numElem**3,2], color=cmap(c))
-			elif c == chainSize-1:
-				ax.scatter(coords[numElem**3*c:,0], coords[numElem**3*c:,1], coords[numElem**3*c:,2], color=cmap(c))
-			else:
-				ax.scatter(coords[numElem**3*c:numElem**3*(c+1),0], coords[numElem**3*c:numElem**3*(c+1),1], coords[numElem**3*c:numElem**3*(c+1),2], color=cmap(c))
-		ax.set_xlabel('x')
-		ax.set_ylabel('y')
-		ax.set_zlabel('z')
-		set_axes_equal(ax, zoom=0.06)
-		#ax.view_init(elev=ax.elev+10., azim=ax.azim)
-		fig.savefig(filename+".png", dpi=300)
-		plt.show()
-		#plt.close(fig)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        cmap = get_cmap(chainSize)
+        for c in range(chainSize):
+            if c == 0:
+                ax.scatter(coords[:numElem**3,0], coords[:numElem**3,1], coords[:numElem**3,2], color=cmap(c))
+            elif c == chainSize-1:
+                ax.scatter(coords[numElem**3*c:,0], coords[numElem**3*c:,1], coords[numElem**3*c:,2], color=cmap(c))
+            else:
+                ax.scatter(coords[numElem**3*c:numElem**3*(c+1),0], coords[numElem**3*c:numElem**3*(c+1),1], coords[numElem**3*c:numElem**3*(c+1),2], color=cmap(c))
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        set_axes_equal(ax, zoom=0.06)
+        #ax.view_init(elev=ax.elev+10., azim=ax.azim)
+        fig.savefig(filename+".png", dpi=300)
+        plt.show()
+        #plt.close(fig)
 
 if __name__ == "__main__":
-	main(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
+    main(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
 
